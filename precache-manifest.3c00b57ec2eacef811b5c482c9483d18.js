a93421dcde2ab24326d4246794b16356db767bd8
Cristian Florez <cflorez@quantumconnexion.com>
2020-09-15 12:50:17 -0500

    Updates

---
+++ precache-manifest.3c00b57ec2eacef811b5c482c9483d18.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7662879072fbbfa1d199aaa841c88a8",
+    "url": "/Portafolio/index.html"
+  },
+  {
+    "revision": "bd0d647c402c0bb597d5",
+    "url": "/Portafolio/static/css/main.b21b94a2.chunk.css"
+  },
+  {
+    "revision": "420b7840747fd8cf6d82",
+    "url": "/Portafolio/static/js/2.e954795d.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Portafolio/static/js/2.e954795d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd0d647c402c0bb597d5",
+    "url": "/Portafolio/static/js/main.e62b4932.chunk.js"
+  },
+  {
+    "revision": "a5166c5545381b6e0a20",
+    "url": "/Portafolio/static/js/runtime-main.c0423103.js"
+  },
+  {
+    "revision": "3d3303fc4ea1caac5af47ea70889352f",
+    "url": "/Portafolio/static/media/BookMe.3d3303fc.png"
+  },
+  {
+    "revision": "735d5cd86827545d993a814f775412d1",
+    "url": "/Portafolio/static/media/Portafolio.735d5cd8.png"
+  },
+  {
+    "revision": "4a8c23476a7c20c5bee2a752a6f96e9e",
+    "url": "/Portafolio/static/media/instagram.4a8c2347.png"
+  },
+  {
+    "revision": "f317b745690e4e2ed4208db086ecdbdd",
+    "url": "/Portafolio/static/media/light.f317b745.jpg"
+  }
+]);
