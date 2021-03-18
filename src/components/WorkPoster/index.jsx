1009f6ee04146d53b909c9384d8fe17222ceb076
Cristian Florez <krisyupher@gmail.com>
2021-03-18 15:13:38 -0500

    add tuap

--- src/components/WorkPoster/index.jsx
+++ src/components/WorkPoster/index.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import QuantumConnexion from "../../static/QuantumConnexion.png";
 import "./WorkPoster.scss";
 const WorkPoster = (props) => {
-  const { title, poster, description, link, date, github } = props;
+  const { title, poster, description, linkView, date, Link } = props;
   return (
-    <a href={link} target="_blank" className="WorkContainer">
+    <a href={linkView} target="_blank" className="WorkContainer">
       <img src={poster} alt={poster} />
       <div className="Work_ContainerData">
         <div className="titleContainer">
-          <a href={github}>
-            <FontAwesomeIcon icon={faGithub} className="FontAwesomeIcon" />
+          <a href={Link}>
+            {Link.startsWith("https://github") ? (
+              <FontAwesomeIcon icon={faGithub} className="FontAwesomeIcon" />
+            ) : (
+              <img
+                className="logo"
+                src={QuantumConnexion}
+                alt="logoQuantumConnexion"
+              />
+            )}
           </a>
           <div>
             <h3>{title}</h3>
--- src/containers/Works.jsx
+++ src/containers/Works.jsx
@@ -2,9 +2,9 @@ import React, { Fragment } from "react";
 import WorkPoster from "../components/WorkPoster";
 import BookMePoster from "../static/BookMe.png";
 import ticTacToe from "../static/tres-en-raya-reglas.jpg";
-import Portafolio from "../static/Portafolio.png";
 import MovieSearcherTMDB from "../static/Movie-Searcher-TMDB.png";
 import Countrys from "../static/Countrys.png";
+import TuAp from "../static/TuAp.png";
 import "../styles/containers/Works.scss";
 const Works = () => {
   return (
@@ -15,34 +15,42 @@ const Works = () => {
             title="BookMe"
             poster={BookMePoster}
             description="Proyecto reaclizado con React, React-router, carrucel funcional con Whirligig y consumiendo APIs con async await"
-            link="https://krisyupher.github.io/BookMe-platzi/"
+            linkView="https://krisyupher.github.io/BookMe-platzi/"
             date="2019"
-            github="https://github.com/krisyupher/BookMe-platzi"
+            Link="https://github.com/krisyupher/BookMe-platzi"
           />
           <WorkPoster
             title="Where in the world?"
             poster={Countrys}
             description="Proyecto reaclizado con React, React-router, carrucel funcional con Whirligig y consumiendo APIs con async await"
-            link="https://krisyupher.github.io/countriesflag/"
+            linkView="https://krisyupher.github.io/countriesflag/"
             date="Jul 2020"
-            github="https://github.com/krisyupher/countriesflag"
+            Link="https://github.com/krisyupher/countriesflag"
           />
           <WorkPoster
             title="tic-tac-toe"
             poster={ticTacToe}
             description="React.js, and css. First attempt at PWA (Progressive Web App) download it from your android."
-            link="https://krisyupher.github.io/tres-en-linea/"
+            linkView="https://krisyupher.github.io/tres-en-linea/"
             date="Nov 2020"
-            github="https://github.com/krisyupher/tres-en-linea"
+            Link="https://github.com/krisyupher/tres-en-linea"
           />
           <WorkPoster
             title="Movie Searcher TMDB"
             poster={MovieSearcherTMDB}
             description="IMDB is a repository of information about movies and series that also provides
             consultation services for fans of the subject."
-            link="https://krisyupher.github.io/Movie-Searcher-TMDB/"
+            linkView="https://krisyupher.github.io/Movie-Searcher-TMDB/"
             date="Dec 2020"
-            github="https://github.com/krisyupher/Movie-Searcher-TMDB"
+            Link="https://github.com/krisyupher/Movie-Searcher-TMDB"
+          />
+          <WorkPoster
+            title="TuAp.com"
+            poster={TuAp}
+            description="web and mobile project - Angular and Ionic - eCommerce for stores - Running right now"
+            linkView="https://TuAp.com"
+            date="Mar 2021"
+            Link="https://www.linkedin.com/company/quantum-connexion"
           />
         </div>
       </div>
